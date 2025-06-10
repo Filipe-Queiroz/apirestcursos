3f47f02964300bef65cbccd0636776d4326dc1a4
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2025-06-10 09:27:15 -0300

    modificando controller

--- src/main/java/br/com/apirest/produtos/controller/CursoController.java
+++ src/main/java/br/com/apirest/produtos/controller/CursoController.java
@@ -1,20 +1,67 @@
 package br.com.apirest.produtos.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@ResponseBody
-@RequestMapping(value="/cursos")
+import br.com.apirest.produtos.entity.Curso;
+import br.com.apirest.produtos.services.CursoService;
+
+@RestController
+@RequestMapping(value="/api/cursos")
 public class CursoController {
-
-	@GetMapping
-	public ModelAndView cursoJava() {
-		ModelAndView template = new ModelAndView("index");
-		
-		return template;
+	
+	private final CursoService cursoService;
+	
+	@Autowired
+	public CursoController(CursoService cursoService) {
+		this.cursoService = cursoService;
 	}
+	
+	@GetMapping
+	public ResponseEntity<List<Curso>> listarTodosCursos() {
+		List<Curso> cursos = cursoService.listarTodosCurso();
+		return ResponseEntity.ok(cursos);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Curso> buscarCursoPorId(@PathVariable Long id) {
+		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
+		return curso.map(ResponseEntity::ok)
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	@PostMapping
+	public ResponseEntity<Curso> adicionarCurso(@RequestBody Curso curso) {
+		
+		
+		
+		
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/br/com/apirest/produtos/services/CursoService.java
+++ src/main/java/br/com/apirest/produtos/services/CursoService.java
@@ -1,5 +1,37 @@
 package br.com.apirest.produtos.services;
 
-public class CursoService {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.apirest.produtos.entity.Curso;
+import br.com.apirest.produtos.repository.CursoRepository;
+
+@Service
+public class CursoService {
+	
+	private final CursoRepository cursoRepository;
+	
+	@Autowired
+	public CursoService(CursoRepository cursoRepository) {
+		this.cursoRepository = cursoRepository;
+	}
+	
+	public Curso salvarCurso(Curso curso) {
+		return cursoRepository.save(curso);
+	}
+	
+	public Optional<Curso> buscarCursoPorId(Long id) {
+		return cursoRepository.findById(id);
+	}
+	
+	public List<Curso> listarTodosCurso() {
+		return cursoRepository.findAll();
+	}
+	
+	public void excluirCurso(Long id) {
+		cursoRepository.deleteById(id);
+	}
 }
