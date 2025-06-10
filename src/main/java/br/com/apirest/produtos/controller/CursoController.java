c22539e71b5f72bc043afe34e603068adf8b7c09
Filipe Queiroz <filipe.queiroz.312@gmail.com>
2025-06-10 15:08:21 -0300

    Atualizando Controller

--- src/main/java/br/com/apirest/produtos/controller/CursoController.java
+++ src/main/java/br/com/apirest/produtos/controller/CursoController.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +35,7 @@ public class CursoController {
 		return ResponseEntity.ok(cursos);
 	}
 	
-	@GetMapping
+	@GetMapping("/{id}")
 	public ResponseEntity<Curso> buscarCursoPorId(@PathVariable Long id) {
 		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
 		return curso.map(ResponseEntity::ok)
@@ -43,8 +46,34 @@ public class CursoController {
 	public ResponseEntity<Curso> adicionarCurso(@RequestBody Curso curso) {
 		
 		
+		Curso novoCurso = cursoService.salvarCurso(curso);
+		return ResponseEntity.status(HttpStatus.CREATED).body(novoCurso);
 		
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Curso> atualizarCurso(@PathVariable Long id, @RequestBody Curso cursoAtualizado) {
+		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
 		
+		if (curso.isPresent()) {
+			cursoAtualizado.setId(id);
+			Curso cursoAtualizadoSalvo = cursoService.salvarCurso(cursoAtualizado);
+			return ResponseEntity.ok(cursoAtualizado);
+		} else {
+			return ResponseEntity.notFound().build();
+		}
+	}
+	
+	@DeleteMapping("/{id}") 
+	public ResponseEntity<Void> excluirCurso(@PathVariable Long id) {
+		Optional<Curso> curso = cursoService.buscarCursoPorId(id);
+		
+		if (curso.isPresent()) {
+			cursoService.excluirCurso(id);
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 }
